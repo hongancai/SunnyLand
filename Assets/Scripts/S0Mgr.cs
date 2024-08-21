2b7885efc4cac92bdc6810110a3255ce29062d88
HongAnCai <w44793458@gmail.com>
2024-08-21 15:37:52 +0800

    istrigger

--- Assets/Scripts/S0Mgr.cs
+++ Assets/Scripts/S0Mgr.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class S0Mgr : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator animator;
     void Start()
     {
         
